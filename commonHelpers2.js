4cf9292914b20d302541c16b247f599eb296a547
megabytos <72892474+megabytos@users.noreply.github.com>
2024-06-23 16:18:31 +0000

    Deploying to gh-pages from @ megabytos/goit-advancedjs-hw-01@61afa6a3ecd9a2bafd0472002846a66348176587 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import{P as a,t as c}from"./assets/vendor-1e54b975.js";const m=document.querySelector("#vimeo-player"),o=new a(m),r="videoplayer-current-time",s=e=>{localStorage.setItem(r,e.seconds)},t=localStorage.getItem(r);t&&o.setCurrentTime(t).catch(e=>{console.error("Error:",e)});o.on("timeupdate",c(s,1e3));
+//# sourceMappingURL=commonHelpers2.js.map
